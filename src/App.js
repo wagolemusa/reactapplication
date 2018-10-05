3af525b9514b150bbfdc85cfa7b409e91c2f84ac
kisumutalent <homiemusa@gmail.com>
2018-10-05 21:09:31 +0300

    create floders & files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <h1>refuge wise</h1>
-
+      </BrowserRouter>
     );
   }
 }
