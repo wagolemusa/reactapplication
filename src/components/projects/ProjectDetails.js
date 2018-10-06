df676613a9f63d0e9f9dd5be21d1bad28272af84
kisumutalent <homiemusa@gmail.com>
2018-10-06 21:47:23 +0300

    create show projectDetails by use of id

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ProjectDetails = (props) => {
+    const id = props.match.params.id;
+
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Project Title -{id}</span>
+                    <p>SanaaConnect is a platform that brings together differe</p>
+                </div>
+                <div className="card-action gret lighten-4 grey-text">
+                    <div>Posted by refuge wise</div>
+                    <div>2nd September, 2am</div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default ProjectDetails;
