d09206ad18c0523b0d5da5d4368b2169830e1313
kisumutalent <homiemusa@gmail.com>
2018-10-08 16:08:02 +0300

    create dummy data and retrive to dashboard

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id: '1', title: 'help me find poech', content: 'blas nlash blash'},
+        {id: '2', title: 'collect all the stars', content: 'I do not understard what is all about'},
+        {id: '3', title: 'Hip Hop', content: 'Hip hop musch started is a cullter'}
+    ]
+}
 
 const projectReducer = (state = initState, action) =>{
     return state
