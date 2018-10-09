8351e1e83ebd94e5b1f3e43e37359ca26b325b68
kisumutalent <homiemusa@gmail.com>
2018-10-09 12:00:25 +0300

    adding data to firebase

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,6 +2,18 @@ export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         
         // make asyc call to database
-        dispatch({ type: 'CREATE_PROJECT', project });
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'homie',
+            authorLastName: 'musa',
+            authorId: 2345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project });
+        }).catch((err)=>{
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+
+        })
     }
 };
