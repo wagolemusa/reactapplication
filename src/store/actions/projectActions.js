e951aa9093fa07818e5c9e8ea2f55e8eed3f68ef
kisumutalent <homiemusa@gmail.com>
2018-10-09 11:06:19 +0300

    connecting firebase to react

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         
         // make asyc call to database
         dispatch({ type: 'CREATE_PROJECT', project });
