e0f8078ba48956c1cef511098c6332a93355437c
kisumutalent <homiemusa@gmail.com>
2018-10-11 15:10:18 +0300

    singin up user with firebase

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,22 @@
 const initState = {}
 
 const authReducer = (state = initState, action) =>{
-    return state
+    switch(action.type){
+        case 'LOGIN_ERROR':
+            console.log('login error')
+            return {
+                ...state,
+                authError: 'Login failed'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('login success')
+            return {
+                ...state,
+                authError: null
+            }
+        default:
+            return state;
+    }
 }
 
 export default authReducer;
