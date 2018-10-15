b201d6957f59b7350255d49c7d3a38b9b1e1c187
kisumutalent <homiemusa@gmail.com>
2018-10-15 19:55:51 +0300

    adding user profile

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const store = createStore(rootReducers,
     compose(
         applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
         reduxFirestore(fbConfig),
-        reactReduxFirebase(fbConfig, {attachAuthIsReady: true})
+        reactReduxFirebase(fbConfig, {useFirestoreForProfile: true, userProfile: 'users', attachAuthIsReady: true})
         )
     );   
 
