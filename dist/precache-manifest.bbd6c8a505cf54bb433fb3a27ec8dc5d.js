141defafbc0863646f68b5a12a332ce7058ba6bf
kisumutalent <homiemusa@gmail.com>
2018-11-02 18:48:52 +0300

    modify readme

---
+++ dist/precache-manifest.bbd6c8a505cf54bb433fb3a27ec8dc5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75dbc815e70c2dcc6be1",
+    "url": "/static/js/main.75dbc815.chunk.js"
+  },
+  {
+    "revision": "397e8551476453f22cd0",
+    "url": "/static/js/1.397e8551.chunk.js"
+  },
+  {
+    "revision": "75dbc815e70c2dcc6be1",
+    "url": "/static/css/main.70a21264.chunk.css"
+  },
+  {
+    "revision": "abc5b9ae74940e7fbb5c2b75eee80907",
+    "url": "/index.html"
+  }
+];
